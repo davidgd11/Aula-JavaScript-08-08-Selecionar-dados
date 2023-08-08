9d9b5eb1d63e3ab6780b4161ba496a43665824d5
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-08 11:44:11 -0300

    Aula JS

---
+++ script.js
@@ -0,0 +1,53 @@
+function calcular (raio){
+    return `Perímetro: ${Math.PI * raio}
+    Área: ${Math.PI * raio}`
+}
+console.log(calcular(10));
+
+
+/*
+function exemplo (num){
+    return num * num
+}
+
+const exemplo=(num)=>{
+    return num * num
+}
+
+const exemplo=(num)=>{return num * num}
+*/
+
+
+
+
+//função normal
+function exemplo (num){
+    return num * num 
+}
+console.log(exemplo(20));
+
+
+//arrow function
+const exemplo1=(num)=>{
+    return num * num
+}
+console.log(exemplo1(20))
+
+
+//arrow função simplificada
+const exemplo2=(num)=>{return num * num}
+console.log(exemplo2(20))
+
+
+function inserir(){
+    let numero = document.getElementById('posicao').value -1
+    let novo = document.getElementById('novo').value
+    document.getElementsByClassName('colocado')[numero].innerHTML=novo
+}
+
+
+function mostrar(){
+    let numero= document.getElementById("numero").value -1
+    let mes= document.getElementsByTagName("li")[numero].innerHTML;
+    document.getElementById("resultado").innerHTML = mes;
+}
